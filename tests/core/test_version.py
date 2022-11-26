8d50f11f809b87adba6bb14583464522fc8e3acb
DuckBoss <jasonjerome16@gmail.com>
2022-11-26 14:47:09 -0500

    Update test_version.py

--- tests/core/test_version.py
+++ tests/core/test_version.py
@@ -1,9 +1,12 @@
 from unittest.mock import patch
 
-from src.version import validate_version
+from src.version import validate_version, version
 
 
 class TestVersion:
+    def test_version_exists(self):
+        assert isinstance(version(), str)
+
     def test_version_is_valid(self):
         with patch("src.version.version", return_value="0.0.1"):
             assert validate_version() is True
