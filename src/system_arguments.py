d26fce1d8dc5d608366b47a8595868961ffdb8b7
DuckBoss <jasonjerome16@gmail.com>
2022-11-26 18:49:23 -0500

    JJMB-54: Improve system_arguments.py coverage

--- src/system_arguments.py
+++ src/system_arguments.py
@@ -8,7 +8,7 @@ args_parser.add_argument(
     "--version",
     help="displays the application version",
     action="version",
-    version=f"Mumimo v{version.VERSION}",
+    version=f"Mumimo v{version.version()}",
 )
 args_parser.add_argument("-hl", "--headless", help="runs the application with no console output", action="store_true")
 args_parser.add_argument("-sh", "--host", help="host IP of murmur server", type=str)
@@ -17,10 +17,10 @@ args_parser.add_argument("-u", "--user", help="the client username", type=str)
 args_parser.add_argument("-p", "--password", help="the client password", type=str)
 args_parser.add_argument("-c", "--certfile", help="path to user certificate", type=str)
 args_parser.add_argument("-k", "--keyfile", help="path to private key associated with user certificate", type=str)
-args_parser.add_argument("-r", "--reconnect", help="attempt to reconnect to the server if disconnected", action="store_true")
 args_parser.add_argument("-t", "--tokens", help="channel access tokens as a list of strings", type=str)
+args_parser.add_argument("-r", "--reconnect", help="attempt to reconnect to the server if disconnected", action="store_true")
 args_parser.add_argument("-d", "--debug", help="enables debug messages to output to the console", action="store_true")
-args_parser.add_argument("-super", "--superuser", help="the user profile that has full access to this client", type=str)
+args_parser.add_argument("-su", "--superuser", help="the user profile that has full access to this client", type=str)
 args_parser.add_argument(
     "-gc",
     "--generate-cert",
---
+++ tests/core/test_system_arguments.py
@@ -0,0 +1,200 @@
+import pytest
+
+from src.system_arguments import args_parser
+
+
+class TestSystemArguments:
+    @pytest.fixture()
+    def sys_args_parser(self):
+        return args_parser
+
+    class TestOptionHeadless:
+        def test_option_headless(self, sys_args_parser):
+            options = ["-hl"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.headless is True
+
+        def test_option_no_headless(self, sys_args_parser):
+            options = []
+            parser = sys_args_parser.parse_args(options)
+            assert parser.headless is False
+
+    class TestOptionHost:
+        def test_option_valid_host(self, sys_args_parser):
+            options = ["-sh", "127.0.0.1"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.host == "127.0.0.1"
+
+        def test_option_invalid_host_value(self, sys_args_parser):
+            options = ["-sh", 99]
+            with pytest.raises(TypeError):
+                sys_args_parser.parse_args(options)
+
+        def test_option_invalid_host_empty(self, sys_args_parser):
+            options = ["-sh"]
+            with pytest.raises(SystemExit):
+                sys_args_parser.parse_args(options)
+
+    class TestOptionPort:
+        def test_option_valid_port(self, sys_args_parser):
+            # The type is an integer, but the input value is a string.
+            # argsparse internally converts it to an integer.
+            options = ["-sp", "64738"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.port == 64738
+
+        def test_option_invalid_port_value(self, sys_args_parser):
+            # The type is an integer, but the input value must be a string.
+            options = ["-sp", 64738]
+            with pytest.raises(TypeError):
+                sys_args_parser.parse_args(options)
+
+        def test_option_invalid_port_empty(self, sys_args_parser):
+            options = ["-sp"]
+            with pytest.raises(SystemExit):
+                sys_args_parser.parse_args(options)
+
+    class TestOptionUser:
+        def test_option_valid_user(self, sys_args_parser):
+            options = ["-u", "testuser"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.user == "testuser"
+
+        def test_option_invalid_user_value(self, sys_args_parser):
+            options = ["-u", 99]
+            with pytest.raises(TypeError):
+                sys_args_parser.parse_args(options)
+
+        def test_option_invalid_user_empty(self, sys_args_parser):
+            options = ["-u"]
+            with pytest.raises(SystemExit):
+                sys_args_parser.parse_args(options)
+
+    class TestOptionPassword:
+        def test_option_valid_password(self, sys_args_parser):
+            options = ["-p", "testpassword"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.password == "testpassword"
+
+        def test_option_invalid_password_value(self, sys_args_parser):
+            options = ["-p", 99]
+            with pytest.raises(TypeError):
+                sys_args_parser.parse_args(options)
+
+        def test_option_invalid_password_empty(self, sys_args_parser):
+            options = ["-p"]
+            with pytest.raises(SystemExit):
+                sys_args_parser.parse_args(options)
+
+    class TestOptionCertFile:
+        def test_option_valid_certfile(self, sys_args_parser):
+            options = ["-c", "path/to/cert"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.certfile == "path/to/cert"
+
+        def test_option_invalid_certfile_value(self, sys_args_parser):
+            options = ["-c", 99]
+            with pytest.raises(TypeError):
+                sys_args_parser.parse_args(options)
+
+        def test_option_invalid_certfile_empty(self, sys_args_parser):
+            options = ["-c"]
+            with pytest.raises(SystemExit):
+                sys_args_parser.parse_args(options)
+
+    class TestOptionKeyFile:
+        def test_option_valid_certfile(self, sys_args_parser):
+            options = ["-k", "path/to/key"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.keyfile == "path/to/key"
+
+        def test_option_invalid_keyfile_value(self, sys_args_parser):
+            options = ["-k", 99]
+            with pytest.raises(TypeError):
+                sys_args_parser.parse_args(options)
+
+        def test_option_invalid_keyfile_empty(self, sys_args_parser):
+            options = ["-k"]
+            with pytest.raises(SystemExit):
+                sys_args_parser.parse_args(options)
+
+    class TestOptionTokens:
+        def test_option_valid_tokens(self, sys_args_parser):
+            options = ["-t", "token1 token2"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.tokens == "token1 token2"
+
+        def test_option_invalid_tokens_value(self, sys_args_parser):
+            options = ["-t", 99]
+            with pytest.raises(TypeError):
+                sys_args_parser.parse_args(options)
+
+        def test_option_invalid_tokens_empty(self, sys_args_parser):
+            options = ["-t"]
+            with pytest.raises(SystemExit):
+                sys_args_parser.parse_args(options)
+
+    class TestOptionReconnect:
+        def test_option_reconnect(self, sys_args_parser):
+            options = ["-r"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.reconnect is True
+
+        def test_option_no_headless(self, sys_args_parser):
+            options = []
+            parser = sys_args_parser.parse_args(options)
+            assert parser.reconnect is False
+
+    class TestOptionDebug:
+        def test_option_debug(self, sys_args_parser):
+            options = ["-d"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.debug is True
+
+        def test_option_no_debug(self, sys_args_parser):
+            options = []
+            parser = sys_args_parser.parse_args(options)
+            assert parser.debug is False
+
+    class TestOptionGenerateCert:
+        def test_option_generate_cert(self, sys_args_parser):
+            options = ["-gc"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.generate_cert is True
+
+        def test_option_no_generatecert(self, sys_args_parser):
+            options = []
+            parser = sys_args_parser.parse_args(options)
+            assert parser.generate_cert is False
+
+    class TestOptionSuperUser:
+        def test_option_valid_superuser(self, sys_args_parser):
+            options = ["-su", "superuser"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.superuser == "superuser"
+
+        def test_option_invalid_superuser_value(self, sys_args_parser):
+            options = ["-su", 99]
+            with pytest.raises(TypeError):
+                sys_args_parser.parse_args(options)
+
+        def test_option_invalid_superuser_empty(self, sys_args_parser):
+            options = ["-su"]
+            with pytest.raises(SystemExit):
+                sys_args_parser.parse_args(options)
+
+    class TestOptionEnvFile:
+        def test_option_valid_env_file(self, sys_args_parser):
+            options = ["-e", "path/to/env"]
+            parser = sys_args_parser.parse_args(options)
+            assert parser.env_file == "path/to/env"
+
+        def test_option_invalid_superuser_value(self, sys_args_parser):
+            options = ["-e", 99]
+            with pytest.raises(TypeError):
+                sys_args_parser.parse_args(options)
+
+        def test_option_invalid_superuser_empty(self, sys_args_parser):
+            options = ["-e"]
+            with pytest.raises(SystemExit):
+                sys_args_parser.parse_args(options)
