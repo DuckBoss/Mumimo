8fe6f45c3c162a71b7326a739450f5570588d44d
Jason Jerome <jasonjerome16@gmail.com>
2023-03-22 01:54:33 -0400

    Updated cleanrun build script to remove pycache files

--- build-tools/cleanrun.sh
+++ build-tools/cleanrun.sh
@@ -11,5 +11,10 @@ if [ -f "$DB_FILE" ] ; then
     echo "Removed '$DB_FILE'"
 fi
 
+# Thanks to V.Gamula/Gulzar on stackoverflow for this convenient command:
+# https://stackoverflow.com/questions/28991015/python3-project-remove-pycache-folders-and-pyc-files
+echo "Removing pycache files..."
+find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
+
 echo "Running Mumimo..."
 ./mumimo.py -vv --env-file='.env' --config-file='.config/config.toml' --log-config-file='.config/logging.toml'
