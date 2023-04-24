110cf98e2babe29860a093cdb253fb45b8e1f6e3
Jason Jerome <jasonjerome16@gmail.com>
2023-04-24 00:31:54 -0400

    Fixed default themes path in gui init service

--- src/services/init_services/gui_init_service.py
+++ src/services/init_services/gui_init_service.py
@@ -2,7 +2,7 @@ import logging
 from typing import Dict, Optional
 
 from ...config import Config
-from ...constants import DEFAULT_PATH_CONFIG_FILE
+from ...constants import DEFAULT_PATH_GUI_THEMES_FILE
 from ...exceptions import ConfigError
 from ...settings import settings
 
@@ -13,19 +13,19 @@ class GUIInitService:
     def __init__(self, sys_args: Dict[str, str]) -> None:
         self._sys_args = sys_args
 
-    def initialize_gui(self, cfg_path: Optional[str] = None) -> "Config":
+    def initialize_gui(self, themes_path: Optional[str] = None) -> "Config":
         # Load in themes from custom themes file path if present.
         # Otherwise load themes from default themes file path.
-        _cfg_instance: Optional["Config"] = settings.configs.get_gui_themes()
-        if _cfg_instance is None:
-            if not cfg_path:
-                cfg_path = DEFAULT_PATH_CONFIG_FILE
-                logger.warning("Mumimo gui config file path not provided. Reading gui config from default path.")
-            _cfg_instance = Config(cfg_path)
-            _cfg_instance.read()
-            settings.configs.set_gui_themes(_cfg_instance)
+        _themes_instance: Optional["Config"] = settings.configs.get_gui_themes()
+        if _themes_instance is None:
+            if not themes_path:
+                themes_path = DEFAULT_PATH_GUI_THEMES_FILE
+                logger.warning("Mumimo gui themes file path not provided. Reading gui themes from default path.")
+            _themes_instance = Config(themes_path)
+            _themes_instance.read()
+            settings.configs.set_gui_themes(_themes_instance)
 
-        _cfg_instance = settings.configs.get_gui_themes()
-        if _cfg_instance is None:
-            raise ConfigError("An unexpected error occurred where the gui config file was not read during initialization.", logger=logger)
-        return _cfg_instance
+        _themes_instance = settings.configs.get_gui_themes()
+        if _themes_instance is None:
+            raise ConfigError("An unexpected error occurred where the gui themes file was not read during initialization.", logger=logger)
+        return _themes_instance
